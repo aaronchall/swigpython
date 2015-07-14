462f1a068529f9ebaba3b9dc638bfeced557e8fa
Aaron Hall, MBA <aaronchall@yahoo.com>
2015-07-13 23:58:36 -0400

    Fix typo

--- example.c
+++ example.c
@@ -3,7 +3,7 @@
 #include "example.h"
 
 int fact(int n) {
-  if (n<0){ /* This should propably return an error, but this is simpler*/
+  if (n<0){ /* This should probably return an error, but this is simpler*/
     return 0;
   }
   if (n == 0) {
